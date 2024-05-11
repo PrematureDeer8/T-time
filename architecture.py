bbfc7271c4aa9ee06680a065c476bab8e9867363
PrematureDeer8 <yankeegabi.torres@gmail.com>
2024-05-10 22:35:34 -0700

    pivot to an dense object detector for text detection

--- architecture.py
+++ architecture.py
@@ -1,59 +1,34 @@
 
-# reimplementation of https://doi.org/10.1145/3356728
+# reimplementation of https://arxiv.org/pdf/1708.02002
 import torch
 from torch import nn
-from torchvision.models import vgg16
+from torchvision.models import resnet50
+import torchvision.ops
 
 
 class T_time(nn.Module):
     def __init__(self):
         super(T_time, self).__init__();
-        # parameters of vgg are initialized by VGG-16 weights
-        vgg = vgg16(weights="DEFAULT");
-        # get rid of last pooling layer (also going to forgo the Adaptive Pooling)
-        # drop the classification layer (FC layers)
-        backbone = vgg.features[:-1];
-        # stage 1 and 2
-        self.stage_1_2 = backbone[:4];
-        # stage 3
-        self.stage3 = backbone[4:9];
-        # stage 4
-        self.stage4 = backbone[9:16];
-        # stage 5
-        self.stage5 = backbone[16:];
-        # MCFE dilated convolution 
-        dilations = [1,3,5,7]; 
-        self.MCFE1 = [nn.Conv2d(in_channels=128, out_channels=64, kernel_size=(3,3),padding=dilation, dilation=dilation) for dilation in dilations];
-        self.MCFE2 = [nn.Conv2d(in_channels=256, out_channels=128, kernel_size=(3,3),padding=dilation, dilation=dilation) for dilation in dilations];
-        self.MCFE3 = [nn.Conv2d(in_channels=512, out_channels=128, kernel_size=(3,3),padding=dilation, dilation=dilation) for dilation in dilations];
+        # feature pyramid network
+        # out channels will be 256
+        self.fpn = torchvision.ops.FeaturePyramidNetwork([512,1024,2048], 256);
+        self.features = {};
+        # resnet backbone
+        self.backbone = resnet50(weights="DEFAULT");
     
     def forward(self, x):
-        x = self.stage_1_2(x); 
-        f3 = self.stage3(x); # --> pass this to MCFE
-        self.MCFE(f3, code=1);
-        f4 = self.stage4(f3); # --> pass this to MCFE
-        self.MCFE(f4, code=2);
-        f5 = self.stage5(f4); # --> pass this to MCFE
-        self.MCFE(f5, code=3);
+        x = self.backbone.conv1(x);
+        x = self.backbone.bn1(x);
+        x = self.backbone.relu(x);
+        x = self.backbone.maxpool(x);
+        x = self.backbone.layer1(x);
 
-    def MCFE(self,feature, code=1):
-        if(code == 1):
-            return torch.concat([self.MCFE1[0](feature),
-                                self.MCFE1[1](feature),
-                                self.MCFE1[2](feature),
-                                self.MCFE1[3](feature)]);
-        elif(code == 2):
-            return torch.concat([self.MCFE2[0](feature),
-                                self.MCFE2[1](feature),
-                                self.MCFE2[2](feature),
-                                self.MCFE2[3](feature)]);
-        elif(code == 3):
-            return torch.concat([self.MCFE3[0](feature),
-                                self.MCFE3[1](feature),
-                                self.MCFE3[2](feature),
-                                self.MCFE3[3](feature)]);
-        else:
-            print("No such code {}!".format(code))
-            raise IndexError;
+        # these features will go into the FPN
+        self.features["feat2"] = self.backbone.layer2(x);
+        self.features["feat3"] = self.backbone.layer3(self.features["feat2"]);
+        self.features["feat4"] = self.backbone.layer4(self.features["feat3"] );
+        
+        self.fpn(self.features);
+
     
 
