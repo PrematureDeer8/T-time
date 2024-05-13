b16d0518bfbd99f61eb4a83865b36082b98fa553
PrematureDeer8 <yankeegabi.torres@gmail.com>
2024-05-12 23:46:15 -0700

    training script

--- coco2014.py
+++ coco2014.py
@@ -4,7 +4,7 @@ import cv2 as cv
 import torch
 
 class CocoDataset2014:
-    def __init__(self, annotation_file, img_folder, shuffle=False) -> None:
+    def __init__(self, annotation_file, img_folder) -> None:
         self.fp = pathlib.Path(annotation_file);
         self.img_folder = pathlib.Path(img_folder);
         with self.fp.open() as file:
@@ -32,35 +32,47 @@ class CocoDataset2014:
     # input:
     #   images (list of tensors) -> tensor: shape [batch_size, channel, height, width]
     #   targets -> [{"boxes": [[1,2,3,4],[5,6,7,8]], "labels": [10, 13] }, ...]
-    def input_loader(self, batch_size, height=640, width=640):
-        img_batch = [];
-        targets = [];
-        img_batch = torch.zeros(batch_size, 3, height, width);
-        # the max number of bounding boxes in one image is 208 
-        bbox_batch = torch.zeros(batch_size, 208, 4);
-        labels = torch.zeros(batch_size, 208);
-        for i in range(batch_size):
-            file = self.img_folder / self.train_imgs[i]["file_name"];
-            img_np = cv.imread(str(file.absolute()));
-            ogh, ogw = img_np.shape[:2];
-            img_np = cv.resize(img_np, dsize=(height, width), interpolation=cv.INTER_LINEAR);
-            # change numpy dim of [3,height, width ] --> [width, height,3]
-            img = torch.from_numpy(img_np).permute(2,1,0);
-            img_batch[i] = img;
-            d = {};
-            for j, box_id in enumerate(self.train_imgs[i]["annotation_ids"]):
-                # should be a list [x1, y1, width, height]
-                bbox = self.annotations["anns"][str(box_id)]["bbox"];
-                if(len(bbox)):
-                    # have to change the bounding box to match
-                    # the resized image
-                    scale_w, scale_h = width/ogw, height/ogh;
-                    x1, y1, x2, y2 = bbox[0] * scale_w, bbox[1] * scale_h,\
-                        (bbox[0] + bbox[2]) * scale_w, (bbox[1] + bbox[3]) * scale_h;
-                    bbox_batch[i][j] = torch.Tensor([x1,y1,x2,y2]);
-                    # change the label to be an object in respective position
-                    labels[i][j] = 1;
-            d["boxes"] = bbox_batch[i];
-            d["labels"] = labels[i];
-            targets.append(d);
-        return img_batch, targets;
+    def input_loader(self, batch_size, height=640, width=640, shuffle=False):
+        for k in range(int(len(self.train_imgs) / batch_size)):
+            print(f"[{(k+1) * batch_size} / {len(self.train_imgs)}]")
+            img_batch = [];
+            targets = [];
+            img_batch = torch.zeros(batch_size, 3, height, width);
+            # the max number of bounding boxes in one image is 208 
+            bbox_batch = torch.zeros(batch_size, 208, 4);
+            # this line is so that pytorch does not freakout
+            # because the x2 > x1  and y2 > y1 or it will through an error
+            bbox_batch[...,2:] = 1;
+            labels = torch.zeros(batch_size, 208);
+            # for when we reach the last part of the dataset
+            # the batch size might not go evenly into the length of the
+            # dataset so we must assign a batch size that will not go over
+            if((k+1) == (int(len(self.train_imgs) / batch_size))):
+                batch_size = len(self.train_imgs) - k * batch_size;
+            for i in range(batch_size):
+                file = self.img_folder / self.train_imgs[i]["file_name"];
+                img_np = cv.imread(str(file.absolute()));
+                ogh, ogw = img_np.shape[:2];
+                img_np = cv.resize(img_np, dsize=(height, width), interpolation=cv.INTER_LINEAR);
+                # change numpy dim of [3,height, width ] --> [width, height,3]
+                img = torch.from_numpy(img_np).permute(2,1,0);
+                img_batch[i] = img;
+                d = {};
+                for j, box_id in enumerate(self.train_imgs[i]["annotation_ids"]):
+                    # should be a list [x1, y1, width, height]
+                    bbox = self.annotations["anns"][str(box_id)]["bbox"];
+                    if(len(bbox)):
+                        # have to change the bounding box to match
+                        # the resized image
+                        scale_w, scale_h = width/ogw, height/ogh;
+                        x1, y1, x2, y2 = bbox[0] * scale_w, bbox[1] * scale_h,\
+                            (bbox[0] + bbox[2]) * scale_w, (bbox[1] + bbox[3]) * scale_h;
+                        bbox_batch[i][j] = torch.Tensor([x1,y1,x2,y2]);
+                        # change the label to be an object in respective position
+                        labels[i][j] = 1;
+                d["boxes"] = bbox_batch[i];
+                d["labels"] = labels[i];
+                targets.append(d);
+            # print(img_batch.shape)
+            # print(type(targets))
+            yield (img_batch, targets);
--- train.py
+++ train.py
@@ -1,17 +1,44 @@
-import torchvision.models.detection.retinanet_resnet50_fpn_v2 as retinanet
+import torchvision
+from coco2014 import CocoDataset2014
+import pathlib
+import torch
 
+#weights path
+W_PATH = pathlib.Path(".") / "architecture" / "state.pt";
+W_PATH.parent.mkdir(parents=True, exist_ok=True);
+W_PATH.touch(exist_ok=True);
 
+# check device
+if(torch.cuda.is_available()):
+    device = ("cuda");
+elif(torch.backends.mps.is_available()):
+    device = ("mps");
+else:
+    device = ("cpu");
 
 def main():
+    print(f"Using device: {device}");
     # no pretrained layers
     # pretrained backbone on ImageNet is fine 
     # 2 classes: Object, and No Object --> text or no text
-    model = retinanet(weights=None, weights_backbone=True, num_classes=2);
+    model = torchvision.models.detection.retinanet_resnet50_fpn(weights=None, weights_backbone=True, num_classes=2).to(device);    
+    # load weight file if possible
+    if(W_PATH.stat().st_size != 0):
+        model.load_state_dict(torch.load(str(W_PATH.absolute())));
     model.train();
 
-    epoch = 1;
+    epochs = 1;
+    dataset = CocoDataset2014("cocotext.v2.json", "train2014");
 
+    print(f"Number of images in training set: {len(dataset.train_imgs)}");
+    dataset_generator = dataset.input_loader(64);
+    for epoch in range(epochs):
+        img, target = next(dataset_generator);
+        _ = model(img.to(device), target);
     
+    
+    torch.save(model.state_dict(), str(W_PATH.absolute()));
+
 
 
 if(__name__ == "__main__"):
