b2ab136b1970ef2af528dad6f9bddd9f6450029d
PrematureDeer8 <yankeegabi.torres@gmail.com>
2024-05-14 16:38:41 -0700

    fpn done

--- retinanet.py
+++ retinanet.py
@@ -4,17 +4,26 @@ from torchvision.models import resnet50, ResNet50_Weights
 class RetinaNet(nn.Module):
     def __init__(self,feature_dim=256) -> None:
         super(RetinaNet, self).__init__();
+        # resnet50
         self.backbone = resnet50(weights=ResNet50_Weights.DEFAULT);
-        # Feature pyramid Network (FPN)
-        self.upsample = nn.Upsample(scale_factor=2, mode="nearest");
-        # reduce channel dimensions of coarser feature map
-        self.conv1x1_2048 = nn.Conv2d(2048, 1024,(1,1));
-        self.conv1x1_1024 = nn.Conv2d(1024, 512, (1,1));
-        self.conv1x1_512 = nn.Conv2d(512, 256, (1,1));
-        # 3x3 convolution for channel dimension reduction
-        self.conv3x3_1024 = nn.Conv2d(1024, feature_dim, (3,3));
-        self.conv3x3_512 = nn.Conv2d(512, feature_dim, (3,3));
-        self.conv3x3_256 = nn.Conv2d(256, feature_dim, (3,3));
+
+        # Feature pyramid Network (FPN) referenced:
+        # https://github.com/yhenon/pytorch-retinanet/blob/master/retinanet/model.py#L19
+        self.conv1x1_C5 = nn.Conv2d(2048, feature_dim, kernel_size=1);
+        self.upsample_P5 = nn.Upsample(scale_factor=2, mode="nearest");
+        self.conv1x1_C4 = nn.Conv2d(1024, feature_dim, kernel_size=1);
+        self.upsample_P4 = nn.Upsample(scale_factor=2, mode="nearest");
+        self.conv1x1_C3 = nn.Conv2d(512, feature_dim, kernel_size=1);
+        self.upsample_P3 = nn.Upsample(scale_factor=2, mode="nearest");
+        self.conv1x1_C2 = nn.Conv2d(256, feature_dim, kernel_size=1);
+        self.conv3x3_P5 = nn.Conv2d(feature_dim, feature_dim, kernel_size=3, padding=1);
+        self.conv3x3_P4 = nn.Conv2d(feature_dim, feature_dim, kernel_size=3, padding=1);
+        self.conv3x3_P3 = nn.Conv2d(feature_dim, feature_dim, kernel_size=3, padding=1);
+        self.conv3x3_P2 = nn.Conv2d(feature_dim, feature_dim, kernel_size=3, padding=1);
+        # for P6
+        self.seq_P6 = nn.Sequential(nn.ReLU(), nn.Conv2d(feature_dim,feature_dim, kernel_size=3,stride=2, padding=1));
+    
+
     def forward(self, x):
 
         x = self.backbone.conv1(x);
@@ -29,6 +38,13 @@ class RetinaNet(nn.Module):
         C5 = self.backbone.layer4(C4);
 
         # top down pathway
-        P4 = self.conv3x3_1024(C4 + self.conv1x1_2048(self.upsample(C5)));
-        P3 = self.conv3x3_512(C3 + self.conv1x1_1024(self.upsample(C4)));
-        P2 = self.conv3x3_256(C2 + self.conv1x1_512(self.upsample(C3)));
+        P5 = self.conv1x1_C5(C5);
+        P4 = self.upsample_P5(P5) + self.conv1x1_C4(C4);
+        P3 = self.upsample_P4(P4) + self.conv1x1_C3(C3);
+        P2 = self.upsample_P3(P3) + self.conv1x1_C2(C2);
+        # 3x3 convolution to reduce aliasing effect from upsampling
+        P5 = self.conv3x3_P5(P5);
+        P4 = self.conv3x3_P4(P4);
+        P3 = self.conv3x3_P3(P3);
+        P2 = self.conv3x3_P2(P2);
+        P6 = self.seq_P6(P5);
